373a7709cf0cfa00c87a75c7fef6fc3f2a58d7db
Tommy Palmer <hi@tommyp.org>
2021-03-05 22:46:43 +0000

    remove word

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,7 @@ import Head from 'next/head'
 import { useState } from 'react'
 import Banner from '../components/banner'
 
-const Text = "type here and fuck off"
-
-
+const Text = "type and fuck off"
 
 export default function Home() {
   const [text, setText] = useState(null)
