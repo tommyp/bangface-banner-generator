c0a7d9d1bdb9f02f1f3215d0b9515f89a612bf22
Tommy Palmer <hi@tommyp.org>
2021-03-08 14:38:47 +0000

    update placeholder

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { useState } from 'react'
 import Banner from '../components/banner'
 
-const Text = "type and fuck off"
+const Text = "type here and fuck off"
 
 export default function Home() {
   const [text, setText] = useState(null)
